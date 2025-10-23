e636ffba2f8d74555400c0d0a557be48c5e3b187
Sangames-B <bsseswaran@outlook.com>
2025-10-23 14:15:29 +0530

    Initial commit

---
+++ Maximum Frequency/Maximum Frequency/maximum Frequency.cs
@@ -0,0 +1,44 @@
+﻿public class Solution 
+{
+    public int LongestBalancedSubarray(int[] nums) 
+    {
+        int maxLength = 0;
+        
+        // Try each possible starting point
+        for (int start = 0; start < nums.Length; start++) 
+        {
+            HashSet<int> evenNums = new HashSet<int>();
+            HashSet<int> oddNums = new HashSet<int>();
+            
+            // Extend the window
+            for (int end = start; end < nums.Length; end++) 
+            {
+                // Add number to appropriate set
+                if (nums[end] % 2 == 0) 
+                {
+                    evenNums.Add(nums[end]);
+                } else 
+                {
+                    oddNums.Add(nums[end]);
+                }
+                
+                // Check if current window is balanced
+                if (evenNums.Count == oddNums.Count) 
+                {
+                    maxLength = Math.Max(maxLength, end - start + 1);
+                }
+            }
+        }
+        
+        return maxLength;
+    }
+}
+
+
+
+
+
+
+
+
+
